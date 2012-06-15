419d3ee3bcd634b5c42c79ef63454c7127a9f1a3
Pascal Gauthier <pgauthier@nihilisme.ca>
2012-06-15 13:48:47 -0400

    Add geoip support

--- weather.py
+++ weather.py
@@ -1,16 +1,72 @@
 #!/usr/bin/python
 
+# Written by Pascal Gauthier <pgauthier@onebigplanet.com>
+# 06.15.2012 
+
 import sys
 import xml.sax
 import urllib2
 import argparse
 
-def fetch_apidata(city, country):
-    "Get data from google weather api"
 
-    google_api = "http://www.google.ca/ig/api?weather=" + city + "," + country
+def fetch_apidata(url):
+    "Get data from API"
 
-    return urllib2.urlopen(google_api) 
+    return urllib2.urlopen(url)
+
+
+class IP2locHandler(xml.sax.ContentHandler):
+    "IP to city location"
+
+    def __init__(self):
+        "Initialize vars"
+
+        self.data = []
+        self.city_name = False 
+        self.country_name = False
+        self.hostip = False
+
+    def startElement(self, name, attrs):
+        "Extract city name and state/province"
+
+        if name == "Hostip":
+            self.hostip = True 
+        if self.hostip and name == "gml:name":
+            self.city_name = True
+
+        if name == "countryAbbrev":
+            self.country_name = True
+
+    def endElement(self, name):
+        "End of element"
+
+        if name == "Hostip":
+            self.hostip = False
+            self.city_name = False 
+
+        if name == "countryAbbrev":
+            self.country_name = False 
+
+    def characters(self, string):
+        "Read element data"
+
+        if self.hostip and self.city_name:
+            self.city = string
+            self.city_name = False
+
+        if self.country_name:
+            self.country = string
+            self.country_name = False
+
+    def locate_city(self):
+        "Return the name of the city"
+
+        if self.city.find("Unknown") != -1 : 
+            print "Unknown city"
+            sys.exit(1)
+
+        return (self.city, self.country)
+
 
 class WeatherHandler(xml.sax.ContentHandler): 
     "Superclass of ContentHandler"
@@ -60,7 +116,8 @@ class WeatherHandler(xml.sax.ContentHandler):
                 self.condition = attrs.getValue("data")
 
             if self.day and self.low and self.high and self.cond:
-                self.forecast_data[self.day] = {'low': self.low , 'high': self.high, 'condition': self.condition}
+                self.forecast_data[self.day] = {'low': self.low , 
+                            'high': self.high, 'condition': self.condition}
 
     def endElement(self, name):
         "End of the element"
@@ -79,6 +136,7 @@ class WeatherHandler(xml.sax.ContentHandler):
 
         if self.city:
             print "\nCurrent condition:"
+            print "------------------\n"
             print "%s: %sc" % (self.city, self.temp)
             print "Condition: %s" % (self.cond)
             print self.humi 
@@ -86,8 +144,12 @@ class WeatherHandler(xml.sax.ContentHandler):
 
             if self.forecast == True:
                 print "\nForecast condition:"
+                print "-------------------\n"
                 for day in self.forecast_data.keys():
-                    print "%s [Low: %d High: %d Condition: %s]" % (day, self.forecast_data[day]['low'], self.forecast_data[day]['high'], self.forecast_data[day]['condition'])
+                    print "%s [Low: %dc High: %dc Condition: %s]" % (day, 
+                        self.forecast_data[day]['low'],
+                        self.forecast_data[day]['high'],
+                        self.forecast_data[day]['condition'])
         else:
             print "No data"
             sys.exit(1)
@@ -99,18 +161,40 @@ def main():
     argparser = argparse.ArgumentParser() 
     argparser.add_argument("--forecast", action="store_true", 
                 help="print current and forcast weather")
-    argparser.add_argument("city", help="name of the city")
-    argparser.add_argument("country", help="name of the country")
+    argparser.add_argument("--city", help="name of the city")
+    argparser.add_argument("--state", help="name of the state/country")
 
     args = argparser.parse_args()
+    if args.city and not args.state:
+        argparser.print_help()
+        sys.exit(1)
 
-    handler = WeatherHandler(args.forecast)
-    xmlparser = xml.sax.make_parser()
+    ip_handler = IP2locHandler()
+    wt_handler = WeatherHandler(args.forecast)
+    ip_parser = xml.sax.make_parser()
+    weather_parser = xml.sax.make_parser()
 
-    xmlparser.setContentHandler(handler)
-    xmlparser.parse(fetch_apidata(args.city, args.country))
+    if not args.city:
+        # Parse info from IP api if no city provided 
+        ip_parser.setContentHandler(ip_handler)
+        ip_parser.parse(fetch_apidata("http://api.hostip.info/?ip="))
+        city, state = ip_handler.locate_city()
 
-    handler.printWeather()
+        google_api = "http://www.google.ca/ig/api?weather=" + city + "," + state 
+
+        # Parse info from Google weather API
+        weather_parser.setContentHandler(wt_handler)
+        weather_parser.parse(fetch_apidata(google_api))
+    else:
+        google_api = "http://www.google.ca/ig/api?weather=" + args.city + "," + args.state
+
+        # Parse info from Google weather API
+        weather_parser.setContentHandler(wt_handler)
+        weather_parser.parse(fetch_apidata(google_api))
+
+    # Print result
+    wt_handler.printWeather()
+
 
 if __name__ == "__main__":
     main()
